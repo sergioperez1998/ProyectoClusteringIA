1e8b8d83a3bc20def5c059969578d00c9619f699
adrmorver <adrian19041996@gmail.com>
2020-06-15 16:24:20 +0200

    Implementación de argparse.

--- consola.py
+++ consola.py
@@ -1,32 +1,88 @@
 from codigo.funcionesAuxiliares import *
 import time
 import sys
+import argparse
 
-iteraciones_prueba = int(sys.argv[1])
+# ...
+parser = argparse.ArgumentParser(
+    prog="Proyecto Clustering IA 2020",
+    description="Interfaz de consola del Proyecto de Clustering IA 2020.",
+    formatter_class=argparse.RawTextHelpFormatter
+)
+parser.add_argument(
+    "-np",
+    "-numeroPruebas",
+    required=True,
+    type=int,
+    help="Número de pruebas a realizar."
+)
+parser.add_argument(
+    "-de",
+    "-datosEntrada",
+    required=True,
+    help="Ruta completa del csv de entrada."
+)
+parser.add_argument(
+    "-i",
+    "-inicializacion",
+    choices=[0, 1],
+    required=True,
+    type=int,
+    help="""Modo de inicialización.
+         Si elige 0 tendrá que introducir a mano el centro y radio de las circunferencias.
+         Si elige el modo 1 se generarán automáticamente."""
+)
+parser.add_argument(
+    "-nc",
+    "-numeroCluster",
+    required=True,
+    type=int,
+    help="Número de cluster iniciales."
+)
+parser.add_argument(
+    "-cp",
+    "-criterioParada",
+    choices=[0, 1],
+    required=True,
+    type=int,
+    help="""Criterio de parada.
+         Si elige 0 tendrá que añadir el parámero -ni con el número de iteraciones a parar.
+         Si elige el modo 1 se utilizará el criterio de similitud de clúster."""
+)
+parser.add_argument(
+    "-ni",
+    "-numeroIteraciones",
+    type=int,
+    help="""Número de iteraciones si se elige el criterio de parada por iteraciones"""
+)
+
+args = parser.parse_args()
+
+iteraciones_prueba = args.np
 estadisticas = []
 
 for i in range(iteraciones_prueba):
     # 0. Leer datos
-    datos_entrada = leer_datos(sys.argv[2])
+    datos_entrada = leer_datos(args.de)
 
-    tipo_input = int(sys.argv[3])
+    tipo_input = args.i
 
-    num_cluster = int(sys.argv[4])
+    num_cluster = args.nc
 
-    criterio_parada = int(sys.argv[5])  # 0 -> numero_iteraciones, 1 -> similitud_cluster
+    criterio_parada = args.cp  # 0 -> numero_iteraciones, 1 -> similitud_cluster
 
     if criterio_parada == 0:
-        numero_iteraciones = int(sys.argv[6])
+        numero_iteraciones = args.ni
     similitud_cluster = True
 
     # 1. Inicialización de circunferencias iniciales (centro y radio de cada una)
     circunferencias = []
     if tipo_input == 0:
-        for n in range(num_cluster):
-            centro_x = input("Introduzca la coordenada x del centro de la circunferencia: ")
-            centro_y = input("Introduzca la coordenada y del centro de la circunferencia: ")
-            radio = input("Introduzca el radio de la circunferencia: ")
-            circunferencias.append(Circunferencia(Punto(float(centro_x), float(centro_y)), float(radio)))
+        for j in range(num_cluster):
+            centro_x = float(input("Introduzca la coordenada x del centro de la circunferencia: "))
+            centro_y = float(input("Introduzca la coordenada y del centro de la circunferencia: "))
+            radio = float(input("Introduzca el radio de la circunferencia: "))
+            circunferencias.append(Circunferencia(Punto(centro_x, centro_y), radio))
     else:
         inicializar_datos(datos_entrada, num_cluster, circunferencias)
     # 2. Repetir (hasta condición de parada)
@@ -42,8 +98,6 @@ for i in range(iteraciones_prueba):
     # 4. Mostrar salidas y gráfica
     # mostrar_resultados(datos_entrada, circunferencias, iteraciones)
 
-print("--- %s seconds ---" % (time.time() - tiempo))
-
 valor = []
 for i in range(len(estadisticas)):
     valor.append(estadisticas[i][0])
@@ -53,6 +107,7 @@ media = sum(e for e in valor) / len(valor)
 index = valor.index(minimo)
 
 print("Número de iteraciones: ", + iteraciones_prueba)
+print("Tiempo de ejecución: %s segundos" % (time.time() - tiempo))
 print("Mínimo de puntos sin asignar: " + str(minimo))
 print("Máximo de puntos sin asignar: " + str(maximo))
 print("Media de puntos sin asignar: " + str(media))
